7616e332181fbdf20624141ed186a97a0b548afc
Mertcan Kahvecioglu <mertcan.khvcoglu@gmail.com>
2025-11-24 18:58:21 +0300

    minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
 
-  <BrowserRouter basename="/repo-adi">
+  <BrowserRouter basename="/LootDrop">
     <App />
   </BrowserRouter>
 )
